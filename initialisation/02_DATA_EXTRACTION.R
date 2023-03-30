0545af6c54be4c43c3b9508ac9d6feb48b935b17
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-03-30 12:49:21 +0200

    path changing

--- initialisation/02_DATA_EXTRACTION.R
+++ initialisation/02_DATA_EXTRACTION.R
@@ -4,18 +4,18 @@ print("Extracting catch data from the GTA database...")
 NC = data.table(dbGetQuery(con_GTA, "SELECT * FROM fact_tables.global_nominal_catch_firms_level0;"))
 
 # Full geo-referenced catch data set
-if (!file.exists("../inputs/data/CA.rds")){
-  CA = data.table(dbGetQuery(con_GTA, "SELECT * FROM fact_tables.global_catch_firms_level0 WHERE year = 2019;"))
-  saveRDS(CA, "../inputs/data/CA.rds")} else CA = readRDS("../inputs/data/CA.rds")
+if (!file.exists(here("/inputs/data/CA.rds"))){
+  CA = data.table(dbGetQuery(con_GTA, "SELECT * FROM fact_tables.global_catch_firms_level0"))
+  saveRDS(CA, here("/inputs/data/CA.rds"))} else {CA = readRDS(here("/inputs/data/CA.rds"))}
 
 # Geo-referenced catch data set by month and 5°x5° grid area
-if (!file.exists("../inputs/data/CA_55M.rds")){
+if (!file.exists(here("/inputs/data/CA_55M.rds"))){
 CA_55M = data.table(dbGetQuery(con_GTA, "SELECT * FROM fact_tables.global_catch_5deg_1m_firms_level0"))
-saveRDS(CA_55M, "../inputs/data/CA_55M.rds")} else CA_55M = readRDS("../inputs/data/CA_55M.rds")
+saveRDS(CA_55M, here("/inputs/data/CA_55M.rds"))} else {CA_55M = readRDS(here("/inputs/data/CA_55M.rds"))}
 
 # Purse seine and baitboat geo-referenced catch data set by month and 1°x1° grid area 
-if (!file.exists("../inputs/data/CA_PS_BB_11M.rds")){
+if (!file.exists(here("/inputs/data/CA_PS_BB_11M.rds"))){
   CA_PS_BB_11M = data.table(dbGetQuery(con_GTA, "SELECT * FROM fact_tables.global_catch_1deg_1m_ps_bb_firms_level0;"))
-  saveRDS(CA_PS_BB_11M, "../inputs/data/CA_PS_BB_11M.rds")} else CA_PS_BB_11M = readRDS("../inputs/data/CA_PS_BB_11M.rds")
+  saveRDS(CA_PS_BB_11M, here("/inputs/data/CA_PS_BB_11M.rds"))} else {CA_PS_BB_11M = readRDS(here("/inputs/data/CA_PS_BB_11M.rds"))}
 
 print("Catch data extracted!")
