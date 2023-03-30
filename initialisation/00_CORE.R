ed1b9d3f372617663b4f78e8830e0b57218036c3
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-03-30 12:34:39 +0200

    path changing with here::

--- initialisation/00_CORE.R
+++ initialisation/00_CORE.R
@@ -1,6 +1,8 @@
 # Clears the environment
 rm(list = ls())
 
+withr::with_dir(here("initialisation"),{
+
 # Includes defaults and helper functions
 source("91_LIBS_EXTERNAL.R")
 source("92_COLORS_SHAPES.R")
@@ -12,3 +14,5 @@ source("01_SPATIAL_LAYERS_READING.R")
 source("02_DATA_EXTRACTION.R")
 source("03_1_MISLOCATION_ON_LAND_IDENTIFICATION.R")
 source("03_2_MISLOCATION_ON_LAND_MAPPING.R")
+
+})
