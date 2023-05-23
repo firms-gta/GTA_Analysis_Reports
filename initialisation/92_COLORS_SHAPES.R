65e3d46bd8aef667b17e020b77d074312a3c09e0
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-05-23 15:52:39 +0400

    adding color shapes

--- initialisation/92_COLORS_SHAPES.R
+++ initialisation/92_COLORS_SHAPES.R
@@ -9,4 +9,16 @@ OCEAN_COL_SHAPE    = data.table(ocean_code = c("AO", "IO", "WCPO", "EPO"), ocean
 # SEX COLORS AND SHAPES
 SEX_COL_SHAPE    = data.table(sex = c("M", "F", "Ind", "unknown"), sex_label = c("Male", "Female", "Indeterminate", "Unknown"), FILL = pal_jco(alpha = 1)(4), OUTLINE = darken(pal_jco(alpha = 1)(4), 0.2), SHAPE = c(1, 2, 3, 4))
 
+RFMO_LEVELS = c("IATTC", "ICCAT", "WCPFC", "IOTC", "CCSBT")
+RFMO_COL    = data.frame(RFMO = RFMO_LEVELS, 
+                         FILL = ggsci::pal_jco("default", alpha = 0.6)(length(RFMO_LEVELS)), 
+                         OUTLINE = colorspace::darken(ggsci::pal_jco("default", alpha = 0.6)(length(RFMO_LEVELS)), 0.2))
+
+# SPECIES
+SPECIES_LEVELS = c("ALB", "BET", "BFT", "SKJ", "YFT")
+SPECIES_COL    = data.frame(SPECIES_CODE = SPECIES_LEVELS, 
+                            FILL = ggsci::pal_simpsons("springfield", alpha = 0.6)(length(SPECIES_LEVELS)), 
+                            OUTLINE = colorspace::darken(ggsci::pal_simpsons("springfield", alpha = 0.6)(length(SPECIES_LEVELS)), 0.2))
+
+
 print("Colors and symbols defined")
