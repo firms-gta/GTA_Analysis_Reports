6e6595d0bc6b851ac4750d00983bee68740616a4
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-05-23 15:51:37 +0400

    minor render add

--- generate_all.R
+++ generate_all.R
@@ -1,6 +1,4 @@
 # Libraries
-renv::activate()
-renv::restore()
 library(rmarkdown)
 library(officedown)
 library(knitr)
@@ -9,4 +7,17 @@ library(knitr)
 source("initialisation/00_CORE.R")
 
 # DOCX
-render("rmd/00_DOCX.Rmd", output_format = rdocx_document(reference_docx = paste0(here(),"templates/doc_template2.docx")), output_file = paste0(here(),"/outputs/GTA_data_checks.docx"))
+render(here("rmd/00_DOCX.Rmd"), output_format = rdocx_document(reference_docx = paste0(here("templates/doc_template2.docx"))), output_file = paste0(here("outputs/GTA_data_checks.docx")))
+
+
+render(here("rmd/NOT_CONFORM_CONVERSION_FACTORS.Rmd"), output_format = "")
+
+render(here("rmd/OVERLAPPING_GENERAL.Rmd"), output_format = rdocx_document(reference_docx = paste0(here("templates/doc_template2.docx"))), output_file = paste0(here("outputs/GTA_overlaps.docx")))
+
+
+
+render(here("rmd/Overlapping IOTC _ WCPFC.Rmd"), output_format = rdocx_document(reference_docx = paste0(here("templates/doc_template2.docx"))), output_file = paste0(here("outputs/GTA_overlaps_IOTC_WCPFC.docx")))
+render(here("rmd/Overlapping IATTC _ WCPFC.Rmd"), output_format = rdocx_document(reference_docx = paste0(here("templates/doc_template2.docx"))), output_file = paste0(here("outputs/GTA_overlaps_IATTC_WCPFC.docx")))
+render(here("rmd/Overlapping_CCSBT_data.Rmd"), output_format = rdocx_document(reference_docx = paste0(here("templates/doc_template2.docx"))), output_file = paste0(here("outputs/GTA_overlaps_SBF.docx")))
+
+
