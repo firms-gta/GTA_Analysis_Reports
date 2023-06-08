6806f86f6cbebfd0501831a937e1d5c744eed41a
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-06-08 15:56:50 +0400

    New species filter based on the outputs of the Global Tuna Atlas

--- initialisation/02_02_SPECIES_KEPT.R
+++ initialisation/02_02_SPECIES_KEPT.R
@@ -1,5 +1,8 @@
-fish_species <- c("Bigeye tuna", "Skipjack tuna", "Yellowfin tuna", "Albacore", "Atlantic bluefin tuna", "Southern Bluefin Tuna", "Pacific bluefin tuna", "Swordfish")
-cwp_codes <- c("BET", "SKJ", "YFT", "ALB","BFT",  "SBF","PBF", "SWO")
+
+fish_GTA <- readr::read_csv("https://raw.githubusercontent.com/fdiwg/fdi-codelists/main/global/firms/gta/cl_species_level0.csv")
+
+fish_species <- unique(fish_GTA$label)
+cwp_codes <- unique(fish_GTA$code)
 
 
-filter_species = FALSE
+filter_species = TRUE
